af7f4be7943254fb2e9ff85641aa4f5634922e9b
Matteo Ruina <matteo.ruina@gmail.comw>
2019-11-26 09:24:00 +0100

    Add root handler

--- cmd/go-infrabin/main_test.go
+++ cmd/go-infrabin/main_test.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
+
+func TestRootHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(RootHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	name, err := os.Hostname()
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := fmt.Sprintf(`{"status": "running", "hostname": "%s"}`, name)
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
 
 func TestLivenessHandler(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
