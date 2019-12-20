4f85dbb446de88d92c0974395755a18a83dee0d6
Matteo Ruina <matteo.ruina@skyscanner.net>
2019-12-20 22:14:27 +0100

    Add TestMarshalStructToString unit test

--- internal/helpers/struct_test.go
+++ internal/helpers/struct_test.go
@@ -1,1 +1,18 @@
 package helpers
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestMarshalStructToString(t *testing.T) {
+	var resp Response
+	resp.Hostname = "hal"
+
+	data := MarshalStructToString(resp)
+	expected := fmt.Sprint(`{"hostname":"hal"}`)
+	if data != expected {
+		log.Fatalf("error: expected %v got %v", expected, data)
+	}
+}
