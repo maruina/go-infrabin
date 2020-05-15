7c942b571dbd85febcb9cb0108b071df1d6a563d
Charlie Gildawie <charlieg@monzo.com>
2020-05-15 15:57:11 +0100

    silly me

--- internal/dns/server.go
+++ internal/dns/server.go
@@ -15,10 +15,6 @@ func init() {
 	miekg.HandleFunc("cname.infrabin.com", CNAMERecordResponse)
 }
 
-var (
-	tcpServer *miekg.Server
-	udpServer *miekg.Server
-)
 
 // Runs a TCP and UDP DNS Server on port {port}
 func RunDefaultDNSServerWithFinChan(port int) (chan struct{}, chan struct{}) {
@@ -29,20 +25,17 @@ func RunDefaultDNSServerWithFinChan(port int) (chan struct{}, chan struct{}) {
 	done := make(chan struct{})
 
 	go func() {
-		s, tcpServerAddress, tcpFinish, err := runLocalTCPServerWithFinChan(fmt.Sprintf(":%d", port))
+		tcpServer, tcpServerAddress, tcpFinish, err := runLocalTCPServerWithFinChan(fmt.Sprintf(":%d", port))
 		if err != nil {
 			panic(err)
 		}
-		// Register TCP Server so we can restart it
-		tcpServer = s
 		log.Printf("TCP DNS Server started on Address: %v\n", tcpServerAddress)
 
-		s, udpServerAddress, udpFinish, err := runLocalUDPServerWithFinChan(fmt.Sprintf(":%d", port))
+		udpServer, udpServerAddress, udpFinish, err := runLocalUDPServerWithFinChan(fmt.Sprintf(":%d", port))
 		if err != nil {
 			panic(err)
 		}
 		// Register UDP Server so we can restart it
-		udpServer = s
 		log.Printf("UDP DNS Server started on Address: %v\n", udpServerAddress)
 
 		// Select over all our signals.
@@ -55,13 +48,11 @@ func RunDefaultDNSServerWithFinChan(port int) (chan struct{}, chan struct{}) {
 			return
 		case err := <-tcpFinish:
 			log.Printf("TCP DNS Server Died unexpectedly! restarting: %v\n", err.Error())
-			_ = tcpServer.Shutdown()
-			tcpServer, tcpServerAddress, tcpFinish, err = runLocalTCPServerWithFinChan(fmt.Sprintf(":%d", port))
+			tcpServer, _, tcpFinish, _ = runLocalTCPServerWithFinChan(fmt.Sprintf(":%d", port))
 
 		case err := <-udpFinish:
 			log.Printf("UDP DNS Server Died unexpectedly! restarting: %v\n", err.Error())
-			_ = udpServer.Shutdown()
-			tcpServer, udpServerAddress, udpFinish, err = runLocalUDPServerWithFinChan(fmt.Sprintf(":%d", port))
+			tcpServer, _, udpFinish, _ = runLocalUDPServerWithFinChan(fmt.Sprintf(":%d", port))
 		}
 	}()
 
