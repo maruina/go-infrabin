2bad110f08f606edb50a22b23fb0482b4ae72963
Alex Williams <smirlie@googlemail.com>
2020-07-01 15:13:04 +0100

    Apply suggestions from code review
    Co-authored-by: Matteo Ruina <matteo.ruina@gmail.com>

--- pkg/infrabin/grpc.go
+++ pkg/infrabin/grpc.go
@@ -14,21 +14,21 @@ type GRPCServer struct {
 	Server *grpc.Server
 }
 
-// Listen binds the server to the indicated interface:port.
+// ListenAndServe binds the server to the indicated interface:port.
 func (s *GRPCServer) ListenAndServe() {
 	ln, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Listen failed on 0.0.0.0:50051: %v", err)
 	}
 
-	log.Printf("Starting %s grpc server on %s", s.Name, ln.Addr())
+	log.Printf("Starting %s server on %s", s.Name, ln.Addr())
 	if err := s.Server.Serve(ln); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
 
 func (s *GRPCServer) Shutdown() {
-	log.Printf("Shutting down %s grpc server with GracefulStop()", s.Name)
+	log.Printf("Shutting down %s server with GracefulStop()", s.Name)
 	s.Server.GracefulStop()
 	log.Printf("GRPC %s server stopped", s.Name)
 }
