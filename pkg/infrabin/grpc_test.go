2663106ed95270b069a6e3fd48718ff5ac8f2484
Alex Williams <alex.williams@skyscanner.net>
2020-10-24 14:54:07 +0100

    Make unittest less flakey

--- pkg/infrabin/grpc_test.go
+++ pkg/infrabin/grpc_test.go
@@ -2,6 +2,7 @@ package infrabin
 
 import (
 	"context"
+	"log"
 	"net"
 	"testing"
 	"time"
@@ -29,7 +30,11 @@ func TestShutdown(t *testing.T) {
 
 	// Create server and client
 	lis := bufconn.Listen(1024 * 1024) // use a bufconn listener for testing
+	// Start server
 	server := NewGRPCServer()
+	go server.ListenAndServe(lis)
+
+	// Create connection, dialier, and client
 	conn, err := grpc.DialContext(
 		context.Background(),
 		"bufnet",
@@ -41,30 +46,31 @@ func TestShutdown(t *testing.T) {
 	}
 	defer conn.Close()
 	client := NewInfrabinClient(conn)
+
 	// We make a channel for errors as we cannot call t.Fatal inside a non test goroutine
 	errs := make(chan error, 1)
 
-	// Start server, call Delay for longer than graceful stop
-	go server.ListenAndServe(lis)
-	// Wait enough time for server to have started
-	time.Sleep(100 * time.Millisecond)
 	start := time.Now()
 	// Call delay with 2 * drainTimeout (1s) in the background
 	go func() {
+		log.Println("Calling Delay gRPC method")
 		if _, err := client.Delay(context.Background(), &DelayRequest{Duration: 1}); err != nil {
 			errs <- err
 		}
 	}()
+	// Make sure Serve and Delay are called
+	time.Sleep(200 * time.Millisecond)
 	// Start the shutdown
 	server.Shutdown()
 	elapsed := time.Since(start).Milliseconds()
 
 	// Check the shutdown waits for at least 500ms, but not the full duration 1s
+	// Should take ~700ms (200ms wait + 500ms drainTimeout)
 	if elapsed < drainTimeout.Milliseconds() || elapsed > 1000 {
-		t.Fail()
+		t.Errorf("Shutdown didn't take expected time. Took %d milliseconds", elapsed)
 	}
 	// There should be an error calling delay as we force stop before
 	if err = <-errs; err == nil {
-		t.Fail()
+		t.Error("The call to Delay didn't error. We expect it to fail as the Shutdown.")
 	}
 }
