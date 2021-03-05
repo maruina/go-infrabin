02a9b5d9b2c315d098a263841c150d93cea0b970
Daniel Iziourov <danmx@users.noreply.github.com>
2021-03-05 09:21:11 +0100

    Additional CLI args (#62)

--- cmd/go-infrabin/main.go
+++ cmd/go-infrabin/main.go
@@ -1,69 +1,13 @@
 package main
 
 import (
-	"flag"
 	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/spf13/viper"
-
-	"github.com/maruina/go-infrabin/pkg/infrabin"
+	"github.com/maruina/go-infrabin/internal/cmd"
 )
 
 func main() {
-	// Create a channel to catch signals
-	finish := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// and SIGTERM (used in docker and kubernetes)
-	// SIGKILL or SIGQUIT will not be caught.
-	signal.Notify(finish, syscall.SIGINT, syscall.SIGTERM)
-
-	// Parse the configuration or set default configuration
-	infrabin.ReadConfiguration()
-
-	proxyEndpoint := viper.GetBool("proxyEndpoint")
-
-	flag.BoolVar(
-		&proxyEndpoint,
-		"enable-proxy-endpoint",
-		false,
-		"If true, enables proxy and aws endpoints",
-	)
-	flag.Parse()
-
-	// run grpc server in background
-	grpcServer := infrabin.NewGRPCServer()
-	go grpcServer.ListenAndServe(nil)
-
-	// run service server in background
-	server := infrabin.NewHTTPServer(
-		"server",
-		infrabin.RegisterInfrabin("/", grpcServer.InfrabinService),
-	)
-	go server.ListenAndServe()
-
-	// run admin server in background
-	admin := infrabin.NewHTTPServer(
-		"admin",
-		infrabin.RegisterHealth("/healthcheck/liveness/", grpcServer.HealthService),
-		infrabin.RegisterHealth("/healthcheck/readiness/", grpcServer.HealthService),
-	)
-	go admin.ListenAndServe()
-
-	// run Prometheus server
-	promServer := infrabin.NewHTTPServer(
-		"prom",
-		infrabin.RegisterHandler("/", promhttp.Handler()),
-	)
-	go promServer.ListenAndServe()
-
-	// wait for SIGINT
-	<-finish
-
-	admin.Shutdown()
-	server.Shutdown()
-	grpcServer.Shutdown()
-	promServer.Shutdown()
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1) //nolint:gomnd // custom error codes wouldn't provide much value
+	}
 }
