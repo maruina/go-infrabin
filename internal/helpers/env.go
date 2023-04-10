b9358633861226c4fe44f304186b12261e599f37
Alex Williams <alex.williams@skyscanner.net>
2023-04-10 22:04:36 +0100

    Use camcel case for variable name

--- internal/helpers/env.go
+++ internal/helpers/env.go
@@ -4,12 +4,12 @@ import "os"
 
 // GetEnv retrieves list of possible environment variables with a default value
 // if the environment variable is missing
-func GetEnv(keys_and_fallback ...string) string {
-	last := len(keys_and_fallback) - 1
-	for _, key := range keys_and_fallback[:last] {
+func GetEnv(keysAndFallback ...string) string {
+	last := len(keysAndFallback) - 1
+	for _, key := range keysAndFallback[:last] {
 		if value, ok := os.LookupEnv(key); ok {
 			return value
 		}
 	}
-	return keys_and_fallback[last]
+	return keysAndFallback[last]
 }
