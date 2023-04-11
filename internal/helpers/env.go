08b78a92d07756b33a3a29416c8bd40c5cef2e3d
Alex Williams <alex.williams@skyscanner.net>
2023-04-11 21:54:28 +0100

    Add ClusterName and Region to response (#286)
    * Add ClusterName and Region to response
    
    * Upgrade golangci-lint to 1.52.2 and address issues
    
    * Upgrade setup-protoc and use token for rate limiting

--- internal/helpers/env.go
+++ internal/helpers/env.go
@@ -2,11 +2,14 @@ package helpers
 
 import "os"
 
-// GetEnv retrieves an environment variable with a default value
+// GetEnv retrieves list of possible environment variables with a default value
 // if the environment variable is missing
-func GetEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+func GetEnv(keysAndFallback ...string) string {
+	last := len(keysAndFallback) - 1
+	for _, key := range keysAndFallback[:last] {
+		if value, ok := os.LookupEnv(key); ok {
+			return value
+		}
 	}
-	return fallback
+	return keysAndFallback[last]
 }
